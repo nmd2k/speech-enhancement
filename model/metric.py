be88133c03c0cfe599cba2b44bf69cb7560b5888
manhdung20112000 <manhdung20112000@gmail.com>
2021-06-03 10:54:22 +0700

    update metric

--- model/metric.py
+++ model/metric.py
@@ -5,7 +5,8 @@ import numpy as np
 import torch
 
 def get_iou_score(outputs, labels):
-    A = labels.squeeze(1).bool()
+    lab = torch.where(labels<0., torch.zeros(1).cuda(), torch.ones(1).cuda())
+    A = lab.squeeze(1).bool()
     pred = torch.where(outputs<0., torch.zeros(1).cuda(), torch.ones(1).cuda())
     B = pred.squeeze(1).bool()
     intersection = (A & B).float().sum((1,2))
